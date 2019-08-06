5fcd73e6ed936af4fd6949a91908fd949279a191
eoganz <jakeemettler@gmail.com>
2019-08-06 13:48:55 -0700

    Created CarouselHelper Class

---
+++ MassageSite/Models/CarouselHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MassageSite.Models
+{
+    public class CarouselHelper
+    {
+        //Haven't integrated yet. Need to make sure I don't need more fields
+        [Key]
+        int number;
+        string link;
+        string errorText;
+        
+
+        public CarouselHelper(string link, string errorText)
+        {
+            this.link = link;
+            this.errorText = errorText;
+        }
+
+
+    }
+}
